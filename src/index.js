d744c3f3f6848644c7af72d84bcee02d30861298
Lukas Siemon <lukas.siemon@gmail.com>
2020-06-16 19:19:59 -0700

    amend: eslint

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 module.exports.wrap = (handler) => (event, context, callback) => {
-  // eslint-disable-next-line no-param-reassign
   context.callbackWaitsForEmptyEventLoop = false;
 
   Promise.resolve(handler(event, context))
